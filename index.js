abae66e5a2f57f0f731c7c5b809e1002653bfa3e
janpadrta <padrta.jan@spssol.cz>
2025-01-30 08:42:38 +0100

    uprava na nacitani jen clanku

--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 const apiUrl = 'https://my-json-server.typicode.com/janpadrta/api_article/';
 
 function load_api() {
-//     https://my-json-server.typicode.com/janpadrta/api_article
-
     fetch(apiUrl + 'Articles')
         .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
+            // if (!response.ok) {
+            //     throw new Error('Network response was not ok ' + response.statusText);
+            // }
             return response.json();
         })
         .then(data => {
@@ -16,31 +14,29 @@ function load_api() {
 
             createTableFromJSON(data);
 
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
         });
+        // .catch(error => {
+        //     console.error('There has been a problem with your fetch operation:', error);
+        // });
 }
 
 async function createTableFromJSON(jsonData) {
-    // Vytvoření tabulky
     let table = "<table border='1'><tr><th>User</th><th>Date</th><th>Title</th><th>Content</th></tr>";
 
-    // // Přidání hlavičky tabulky
     // for (let key in jsonData[0]) {
     //     table += `<th>${key}</th>`;
     // }
     // table += "</tr>";
 
-    // Přidání dat do tabulky
     for (const item of jsonData) {
         table += "<tr>";
         // for (let key in item) {
         //     table += `<td>${item[key]}</td>`;
         // }
-        const email = await getUser(item['user_id']);
+        // const email = await getUser(item['user_id']);
 
-        table += `<td>${email}</td>`;
+        // table += `<td>${email}</td>`;
+        table += `<td>${item['user_id']}</td>`;
         table += `<td>${item['date']}</td>`;
         table += `<td>${item['title']}</td>`;
         table += `<td>${item['content']}</td>`;
@@ -51,22 +47,22 @@ async function createTableFromJSON(jsonData) {
     document.getElementById("tableDiv").innerHTML = table;
 }
 
-function getUser(id) {
-    console.log(apiUrl + 'Users/' + id);
-    fetch(apiUrl + 'Users/' + id)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            console.log("-------------------------------------")
-            return data["email"];
-
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
-}
+// async function getUser(id) {
+//     console.log(apiUrl + 'Users/' + id);
+//     fetch(apiUrl + 'Users/' + id)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok ' + response.statusText);
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             console.log(data);
+//             console.log("-------------------------------------")
+//             return data["email"];
+//
+//         })
+//         .catch(error => {
+//             console.error('There has been a problem with your fetch operation:', error);
+//         });
+// }
